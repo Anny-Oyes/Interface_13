bdfa60002225450f140e84895e4d6fb8bb3875a5
Anny-Oyes <stephanyoyes@gmail.com>
2022-10-16 14:33:50 -0600

    Replicando la interfaz número trece

---
+++ components/InterfaceThirteen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function InterfaceThirteen() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
